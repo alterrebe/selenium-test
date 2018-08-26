7646d50125a8b5588123292093f0a7916731193d
Uri Savelchev <uri.savelchev@zalando.fi>
2018-08-27 01:19:30 +0300

    Parameterization

--- src/main/java/test/selenium/Login1.java
+++ src/main/java/test/selenium/Login1.java
@@ -1,6 +1,7 @@
 package test.selenium;
 
-import java.util.concurrent.TimeUnit;
+import java.io.InputStream;
+import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,42 +10,90 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import org.apache.log4j.Logger;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class Login1 {
-    public static void main(String[] args) {
-        final Logger log = Logger.getLogger(Login1.class);
+    private static final Logger LOG = Logger.getLogger(Login1.class);
+    public static final String PROPERTY_FILE = "/vodafone-test.properties";
+
+    private static final Properties PROPERTIES = new Properties();
+    static {
+        InputStream propertyStream = Login1.class.getResourceAsStream(PROPERTY_FILE);
+        try {
+            PROPERTIES.load(propertyStream);
+            propertyStream.close();
+        }
+        catch (Exception e) {
+            LOG.fatal("Can't load application property. Is the file '" + PROPERTY_FILE + "' in classpath?", e);
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        LOG.info("Start processing");
 
 // Create a new instance of the Chrome driver
         WebDriver driver = new ChromeDriver();
+
 // Wait For Page To Load
 // Put a Implicit wait, this means that any search for elements on the page could take the time the implicit wait is set for before throwing exception
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-// Navigate to URL
-        driver.get("https://www.vodafone.cz/en");
-        log.info("Opened the website...");
+        driver.manage().timeouts().implicitlyWait(60, SECONDS);
+
+// Step1. Navigate to URL
+        driver.get( PROPERTIES.getProperty("login.url") );
+        driver.manage().timeouts().implicitlyWait(60, SECONDS);
+
+        LOG.info("Opened the website...");
 // Maximize the window.
         driver.manage().window().maximize();
 // Click on 'My Vodafone' button
         WebElement arrow = driver.findElement(By.className("-arrow"));
-        log.info("Found arrow: " + arrow.getTagName());
+        LOG.info("Found arrow: " + arrow.getTagName());
         arrow.click();
+        //driver.findElement(By.className("vf-service-menu__item -wsc")).click();
 
 // Find the form:
         WebElement loginForm = driver.findElement(By.className("vf-nav-login"));
-        log.info("Found login form: " + loginForm.getTagName());
+        LOG.info("Found login form: " + loginForm.getTagName());
 
-// Enter Phone number
-        driver.findElement(By.id("Phone number")).sendKeys("773010016");
-// Enter Password
-        driver.findElement(By.id("Password")).sendKeys("2013");
-// Click on 'Continue' button
-        driver.findElement(By.id("Continue")).click();
+// Step2. Enter Phone number
+        driver.findElement(By.id("phnr")).sendKeys(PROPERTIES.getProperty("phone.number"));
+
+// Step2. Enter Password
+        driver.findElement(By.id("pwd")).sendKeys(PROPERTIES.getProperty("site.password"));
+
+// Step2. Click on 'Continue' button
+        driver.manage().timeouts().implicitlyWait(120, SECONDS);
+        driver.findElement(By.name("btn")).click();
+        //driver.findElement(By.className("button actionButton bigButton")).click();
+
 // Wait For Page To Load
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(120, SECONDS);
+        Thread.sleep(10000); // ???????
 
-//Click on 'Logout' Button
-        driver.findElement(By.xpath("//*[@id='gb_71']")).click();
+//Step3. Log Kredit pro číslo / Credit for telephone number  - I'm not sure what it should be doing. I've added the only link with "Credit" word in it.
+        driver.get( PROPERTIES.getProperty("credit.url") );
+        driver.manage().timeouts().implicitlyWait(180, SECONDS);
+
+//Step4. Go to My services – Calling & SMS
+        driver.get( PROPERTIES.getProperty("sms.url") );
+        driver.manage().timeouts().implicitlyWait(180, SECONDS);
+
+//Step5 is skipped, because phone number is selected by default and there is only one number.
+        // ???? what if there are other number? I believe it is expected from you to navigate the number...
+
+//Step6. Unfold Missed Call Allert
+        driver.findElement(By.id("missed_call_alert")).click();
+        driver.manage().timeouts().implicitlyWait(180, SECONDS);
+
+//Step6. Select Turnoff/Active buttons
+        WebElement userName = driver.findElement(By.cssSelector("input#username"));
+        driver.findElement(By.name("missed_call_alert_service[save]")).click();
+
+//Step 7. Click on 'Logout' Button
+//        driver.findElement(By.xpath("//*[@id='gb_71']")).click();
+
 //Close the browser.
-        driver.close();
+//        driver.close();
     }
 }
